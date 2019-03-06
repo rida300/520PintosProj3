84388fb65ab1a9e9ec49a43c15fec4e11d244ef4
rida300 <31077599+rida300@users.noreply.github.com>
2019-03-05 22:31:01 -0600

    7 tests failed

--- cis520/pintos/src/userprog/syscall.h
+++ cis520/pintos/src/userprog/syscall.h
@@ -1,6 +1,10 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
-
+#include "lib/user/syscall.h"
 void syscall_init (void);
+
+bool sys_create (const char *, unsigned);
+bool sys_remove (const char *);
+unsigned sys_tell (int fd);
 
 #endif /* userprog/syscall.h */
